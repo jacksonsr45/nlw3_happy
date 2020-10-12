c83a58deb920484f2c51902984d308743407448b
jacksonsr45 <jacksonsr45@gmail.com>
2020-10-12 19:59:21 -0300

    feat(Create-app-Happy): Create App and first page landing

---
+++ web/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
+import arrowRight from './images/arrow-right.svg'
+
+function App() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+        <main>
+          <h1>
+            Leve felicidade para o mundo
+          </h1>
+          <p>
+            Visite orfanatos e mude o dia de muitas crianças.
+          </p>
+          <div className="location">
+            <strong>Jacarezinho</strong>
+            <span>Paraná</span>
+          </div>
+          <a className="enter-app" href="">
+            <img src={arrowRight} alt="App"/>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
